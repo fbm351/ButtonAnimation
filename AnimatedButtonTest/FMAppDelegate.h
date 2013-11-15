9a73f12de042493aa11a7311c4f90577b37a2eb7
Fred Myers <fred@fred-myers.com>
2013-11-15 12:14:51 -0500

    Initial Commit

---
+++ AnimatedButtonTest/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  AnimatedButtonTest
+//
+//  Created by Fredrick Myers on 11/15/13.
+//  Copyright (c) 2013 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AnimatedButtonTest/FMViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMViewController.h
+//  AnimatedButtonTest
+//
+//  Created by Fredrick Myers on 11/15/13.
+//  Copyright (c) 2013 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMViewController : UIViewController
+
+@end
